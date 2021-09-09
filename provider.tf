4f59405720e23435da92df9a199a927c0e6aa8ec
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-09-09 18:26:59 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+provider "aws" {
+    access_key = "#"
+    secret_key = "#"
+    region = "ap-south-1"
+}
